de99b1f6fd86a61d7ddf0197d74892cb28c5077b
Nicholas Bester <nick.bester@hellocomputer.com>
2019-04-10 10:21:52 +0200

    Added environment variable

--- src/main.js
+++ src/main.js
@@ -1,14 +1,39 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import Vuetify from 'vuetify'
-import VueYouTubeEmbed from 'vue-youtube-embed'
+import Vue from "vue";
+import "./plugins/vuetify";
 
-Vue.use(Vuetify)
-Vue.use(VueYouTubeEmbed)
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import VueYouTubeEmbed from "vue-youtube-embed";
+import VueGtm from "vue-gtm";
+import VueRouter from "vue-router";
+import * as firebase from "firebase";
+import VueFirebase from "vue-firebase";
 
-Vue.config.productionTip = false
+// Config firebase project
+const FBCONFIG = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+const router = new VueRouter({ routes, mode, linkActiveClass });
+
+Vue.use(VueGtm, {
+  id: "GTM-MV64F7V",
+  enabled: true,
+  debug: true,
+  vueRouter: router,
+  ignoredViews: []
+});
+Vue.use(VueFirebase, { firebase: firebase, config: FBCONFIG });
+Vue.use(Vuetify);
+Vue.use(VueYouTubeEmbed);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
